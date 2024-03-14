f2e79b5dae27677137898fe82f5903957e4c70eb
erinius <timme003@csusm.edu>
2024-03-14 09:05:30 -0700

    program that generates word lists
    all the words in each list have the same length

---
+++ src/MakeWordLists/Program.fs
@@ -0,0 +1,11 @@
+﻿module MakeWordLists
+
+open System.IO
+open Words
+
+let wordsByLength = 
+    GetWords
+    |> Seq.groupBy (fun w -> w.Length)
+
+for (length, words) in wordsByLength do
+    File.WriteAllLines($"../../word_lists/words_length_{length}.txt", words)
