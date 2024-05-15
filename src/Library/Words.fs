5844f04c43785998026099a2e07894329d32459e
erinius <timme003@csusm.edu>
2024-05-14 23:51:05 -0700

    cache

--- src/Library/Words.fs
+++ src/Library/Words.fs
@@ -13,7 +13,7 @@ let wordToBitfield (w: string) : int =
     bits
 
 let GetWordsFromFile path =
-    File.ReadLines path |> Seq.map (fun w -> w.ToUpperInvariant())
+    File.ReadLines path |> Seq.map (fun w -> w.ToUpperInvariant()) |> Seq.cache
 
 let wordListsDir = Path.Join(__SOURCE_DIRECTORY__, "..", "..", "word_lists")
 let mainListPath = Path.Join(wordListsDir, "words_alpha.txt")
@@ -21,4 +21,4 @@ let mainListPath = Path.Join(wordListsDir, "words_alpha.txt")
 let GetWords = GetWordsFromFile mainListPath
 
 let wordsOfLength (length: int) =
-    GetWordsFromFile(Path.Join(wordListsDir, $"words_length_{length}.txt"))
+    GetWordsFromFile (Path.Join(wordListsDir, $"words_length_{length}.txt"))
